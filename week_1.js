29e4b513d092cf6da0ed58575557ffa4b8d39501
hikma0803 <hikmathun0803@gmail.com>
2024-10-02 17:06:53 +0530

    final draft

--- week_1.js
+++ week_1.js
@@ -49,3 +49,203 @@ block()
 //console.log(w) //throw an error 
 //console.log(i)  //throw an error
 
+//hoisting
+//let,const are TDZ
+function hoisting(){
+    //console.log(w)  //Uncaught ReferenceError: Cannot access 'w' before initialization
+    let w
+    console.log(w)  //undefined
+    //console.log(oiu)  //week_1.js:58 Uncaught ReferenceError: oiu is not defined
+}
+hoisting()
+///hoisting is useful in functions
+sub(10,8)
+function sub(a,b){
+    console.log(a-b)
+}
+
+//mus()  //error
+let mus=function(a,b){
+    console.log(a+b)    //fuctional expression
+} 
+mus(9,8)
+
+
+
+//primitive  datatypes
+Number
+BigInt
+Symbol
+Boolean
+undefined
+null
+String
+
+//objects
+Array
+
+let y=900
+console.log(typeof(y))  //typpe of variable
+
+//precendence left to right
+//except expo right to left
+
+console.log(2-5**7**0+90)
+console.log('2' + 4)//concatenate for +
+console.log('2'*4)//other than + the sting will be converted to num
+console.log(2+2+'2')
+console.log(2+2+true)
+console.log(2+2+false)
+console.log(2*undefined) //anything to undefined ==> NaN
+
+//and &&
+//or ||
+//not !
+
+//if(){}
+//else if (){}
+//else(){}
+ 
+let arr=[1,3,5,'a']
+for (let i in arr){     //index
+    console.log(i)
+}
+
+for (let i of arr){     //item
+    console.log(i)
+}
+
+const obj={'1':'oiu',
+    it: function(a,b){
+        console.log(a+b)
+    }
+} //key-->string
+obj.time=10
+console.log(obj)
+//obj={}//error
+
+for (let item in obj){
+    console.log(obj[item])
+
+}
+console.log(obj.time)
+obj.it(8,9)
+
+
+let obj1={
+    name1:'oiup',
+    name2:function(a,b){
+        console.log(this.name1+a+b)     //this referes to the property inside the object
+    }
+}
+obj1.name2('a','b')
+const obj2 = {
+    fruit: 'apple',
+    price: 50,
+    // say() {
+    // },
+    // say : function say(){
+
+    // },
+    say() {
+        console.log(`${this.fruit} is priced ${this.price}`)
+    },
+    sayArrow: () => {
+        console.log(`${this.fruit} is priced ${this.price}`)
+    }
+}
+
+this.fruit = 'orange'
+
+
+
+obj3 = {
+    fruit: 'kiwi',
+    price: 150,
+}
+
+// call bind and apply
+
+obj2.say.call(obj3)
+
+function add3(z, x) {
+    console.log(this.x + this.y + z + x) }
+
+obj5 = {
+    x: 10,
+    y: 20, }
+
+add3.call(obj5, 30, 50)
+add3.apply(obj5, [30, 50])
+
+let bindedFunc = add3.bind(obj5, 10, 20)
+bindedFunc()
+
+// we cant change the context of arrow using
+// call bind or apply
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

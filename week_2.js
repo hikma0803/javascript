90b8e7117984f4d7f67f706b5e6d0caa5a11bc56
hikma0803 <hikmathun0803@gmail.com>
2024-10-07 22:11:17 +0530

    still process

---
+++ week_2.js
@@ -0,0 +1,25 @@
+var x=10
+var y='9'
+var z='a'
+//it will have string concatenate with '+' sign 
+console.log(x+y) 
+
+// for other math operations it try to convert string to num 
+console.log(x-y)    
+console.log(y-true)
+
+//if the string cannot be converted it will become NaN
+console.log(x-z)    
+ 
+// window
+var o=9
+console.log(o)      //variable
+console.log(window.o)   //property
+console.log(window)     //window
+
+let a=9
+let b='9'
+console.log(a==b) //true
+console.log(a===b)//false
+
+
